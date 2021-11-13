8f54d44a2d26a0b1467dbd898f10293c471b45f0
Asurazzz <2513667783@qq.com>
2021-11-13 14:14:53 +0800

    edu-config

---
+++ edu-config-boot/src/main/java/com/lagou/edu/LagouConfigApplication.java
@@ -0,0 +1,14 @@
+package com.lagou.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class LagouConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagouConfigApplication.class);
+    }
+}
